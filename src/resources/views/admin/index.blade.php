3809c75d421c0deef697b7588cbed98ef130af74
Nasko <nasko@webfactory.bg>
2016-08-23 11:05:15 +0300

    table loading

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -63,7 +63,7 @@
                 </tr>
             </thead>
 
-            <tbody>
+            <tbody ng-class="{'table-loading':isLoading}">
                 <tr ng-repeat="model in displayedModels">
                     <td typi-btn-delete action="delete(model)"></td>
                     <td>
